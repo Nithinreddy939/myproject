79aab31bad0176688be4896e1ca0c501f6f8d7bb
Nithinreddy <nithind279@gmail.com>
2025-03-04 08:42:11 +0530

    changesfiles

--- manage.py
+++ manage.py
@@ -9,12 +9,12 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as exce:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        ) from exce
     execute_from_command_line(sys.argv)
 
 
